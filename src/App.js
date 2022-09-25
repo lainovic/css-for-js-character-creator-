dcc72f6144870faea8f9d5983c5879efa9854a36
Marko Lainovic <marko.lainovic@gmail.com>
2022-09-25 20:51:25 +0200

    Add corrections.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,6 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <div
-        style={{
-          position: "fixed",
-          top: "60%",
-          width: "100%",
-          height: "400px",
-          backgroundColor: "hsl(195deg, 20%, 86%)",
-          zIndex: -1,
-        }}
-      ></div>
       <CharacterEditor />
       <Footer />
     </>
