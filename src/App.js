9f5d98d0fa9ddf727257cb410ee1d619f9bda7e1
Marko Lainovic <marko.lainovic@gmail.com>
2022-09-25 19:55:00 +0200

    The first iteration.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          position: "fixed",
+          top: "60%",
+          width: "100%",
+          height: "400px",
+          backgroundColor: "hsl(195deg, 20%, 86%)",
+          zIndex: -1,
+        }}
+      ></div>
       <CharacterEditor />
       <Footer />
     </>
